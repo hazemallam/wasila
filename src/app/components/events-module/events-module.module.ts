24f8e650135bbceceb3adc513381abed6a53711f
Hazem Allam <s.enghazemallm@gmail.com>
2021-02-01 16:51:00 +0200

    refactoring paths

--- src/app/components/events-module/events-module.module.ts
+++ src/app/components/events-module/events-module.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EventsComponent } from './events/events.component';
-import { SingleEventsComponent } from './single-events/single-events.component';
+import { EventsComponent } from '././events/events.component';
+import { SingleEventsComponent } from '././single-events/single-events.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes : Routes = [
