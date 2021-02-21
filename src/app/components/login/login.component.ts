bb51e6328b5d85037b287361f585e1b874064ca6
Engy Reda <engy.reda.s333@gmail.com>
2021-02-21 11:40:11 -0800

    athentication by engy reda

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LoginService } from 'src/app/Services/Login/login.service';
+import { RegisterService } from 'src/app/Services/Register/register.service';
+import { Register } from "src/app/ViewModels/Register/register";
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +13,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  usersList: Register[]=[];
+  subscribtion: Subscription|null=null;
+  loginFrm: FormGroup = new FormGroup({});
+  NewUser:Register;
 
-  constructor() { }
+  constructor(private prdService: RegisterService , private loginService : LoginService,private fb: FormBuilder
+    , private router:Router ) { 
+      this.NewUser={name:"", email:"",password:""}
+    }
+
 
   ngOnInit(): void {
-  }
+    this.subscribtion= this.prdService.getAllUserRegister().subscribe(
+      (response)=>{
+        console.log("in subscribe");
+        this.usersList=response;
+      },
+      (err)=>{console.log(err)}
+    );
 
+    console.log("After subscribe")
+
+
+    this.loginFrm = this.fb.group({
+      Name: ['', [Validators.required, Validators.minLength(2)]],
+      Email: ['', [Validators.required, Validators.email, Validators.minLength(7)]]
+      , Password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+  }
+  checkLogin()
+  {
+    let count;
+    let x =1;
+
+    console.log(this.NewUser)
+    console.log(this.usersList)
+    for (var i=0; i < this.usersList.length; i++) {
+      if ((this.usersList[i].email === this.NewUser.name) || (this.usersList[i].password === this.NewUser.password)) {
+        console.log("done")
+        this.router.navigate(['/home']);
+        this.login();
+       
+      }
+      else{
+        console.log("false");
+        console.log(this.usersList.length)
+        console.log(x)
+      if(x== this.usersList.length){
+        count = 2;
+        console.log("hhh")
+      
+      }
+      ++x;
+
+      }
+      
+  }
+  if(count == 2)
+         alert("من فضلك لا يوجد لديك حساب . سجل الان ");
+
+    }
+
+    login(){
+      // alert("In Login")
+      this.loginService.login('TestToken');
+      // this.userloginserve.login(this.NewUser.Email)
+    }
+  
+    Logout(){
+      // alert("logout");
+      this.loginService.logout();
+    }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {RegisterService } from 'src/app/Services/Register/register.service';
+import { Register } from 'src/app/ViewModels/Register/register';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
+  NewUser:Register;
+  constructor(private router: Router , private userRegisterserve:RegisterService ) { 
+    this.NewUser={name:"", email:"",password:""} }
   ngOnInit(): void {
   }
+  
+  addUsersReg(){
+    // const prd: IProduct = {ID: 0, Name: 'Assiut PD-40 Test', Quantity: 37, Price: 100};
+    console.log(this.NewUser);
+    this.userRegisterserve.addUserRegister(this.NewUser).subscribe(
+        res => {
+          this.router.navigateByUrl('/home'); console.log(this.NewUser);
+          this.login();
+      },
+        
+        err => {console.log(err); }
+      );
+  }
+  login(){
+    // alert("In Login")
+    this.userRegisterserve.login('TestToken');
+    // this.userloginserve.login(this.NewUser.Email)
+  }
+
+  Logout(){
+    // alert("logout");
+    this.userRegisterserve.logout();
+  }
+
 
 }
