422c01baa3e53457a118bdf6b49d3e1015aa4661
Engy Reda <engy.reda.s333@gmail.com>
2021-02-21 04:38:51 -0800

    authentication by engy

---
+++ src/app/Services/login/login.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Login } from '../../ViewModels/login';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { 
+    
+  }
+  getAllUserLogin(): Observable <Login[]>
+  {
+    return this.http.get<Login[]>(`${environment.URL}/users`);
+  }
+
+  
+  login(Token: string){
+    // alert (Token);
+    localStorage.setItem('UserToken', Token);
+  }
+
+  logout(){
+    localStorage.removeItem('UserToken');
+  }
+
+  isLogged(): boolean {
+    if (localStorage.getItem('UserToken')) {
+      return true ;
+      
+    }
+    else {
+      return false;
+    }
+  }
+  addUserLogin(prd: Login):Observable<any>
+{
+  console.log(prd);
+  const httpOptions = {headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+    //,'Accept':' */*'
+    //,'Authorization': 'my-auth-token'
+      })};
+    
+  return this.http.post<any>(`${environment.URL}/users`,prd, httpOptions);
+}
+}
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {RegisterService } from 'src/app/Services/register/register.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Register } from 'src/app/ViewModels/register';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  NewUser:Register;
+   loginFrm: FormGroup = new FormGroup({});
 
-  constructor() { }
+  constructor(private router: Router , private userRegisterserve:RegisterService ,private fb: FormBuilder) { 
+    this.NewUser={name:"", email:"",password:""} }
 
   ngOnInit(): void {
+    this.loginFrm = this.fb.group({
+      name: ['', [Validators.required, Validators.email, Validators.minLength(15)]]
+      ,email: ['', [Validators.required, Validators.email, Validators.minLength(7)]]
+      , Password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+  }
+  addUsersReg(){
+    // const prd: IProduct = {ID: 0, Name: 'Assiut PD-40 Test', Quantity: 37, Price: 100};
+    console.log(this.NewUser);
+    this.userRegisterserve.addUserRegister(this.NewUser).subscribe(
+        res => {
+          this.router.navigateByUrl('/home'); console.log(this.NewUser);
+          this.login();
+      },
+        
+        err => {console.log(err); }
+      );
+  }
+  login(){
+    // alert("In Login")
+    this.userRegisterserve.login('TestToken');
+    // this.userloginserve.login(this.NewUser.Email)
   }
 
+  Logout(){
+    // alert("logout");
+    this.userRegisterserve.logout();
+  }
+
+
 }
+
+
+
