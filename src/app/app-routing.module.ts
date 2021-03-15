0a4507159c36a316e8d63451d1257b258e960b81
Hazem Allam <s.enghazemallm@gmail.com>
2021-03-16 01:59:41 +0200

    handling errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RegisterComponent } from './Components/register/register.component';
-import { PrivacyComponent } from './Components/privacy/privacy.component';
-import { ContactUsComponent } from './Components/contact-us/contact-us.component';
-import { CoursesComponent } from './Components/courses/courses.component';
-import { MotionGraphicComponent } from './Components/motion-graphic/motion-graphic.component';
-import { ConceptArtComponent } from './Components/concept-art/concept-art.component';
-import { LoginComponent } from './Components/login/login.component';
-import { GraphicDesignComponent } from './Components/graphic-design/graphic-design.component';
-import { ThreedArtComponent } from './Components/threed-art/threed-art.component';
-import { AlumniComponent } from './Components/alumni/alumni.component';
-import { HomeComponent } from './Components/home/home.component';
-import { BlogComponent } from './Components/blog/blog.component';
-// import { QuestionsComponent } from './Components/questions/questions.component';
-import { VideoResourcesComponent } from './Components/video-resources/video-resources.component';
-import { SoundResourcesComponent } from './Components/sound-resources/sound-resources.component';
-import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { MotionGraphicComponent } from './components/motion-graphic/motion-graphic.component';
+import { ConceptArtComponent } from './components/concept-art/concept-art.component';
+import { LoginComponent } from './components/login/login.component';
+import { GraphicDesignComponent } from './components/graphic-design/graphic-design.component';
+import { ThreedArtComponent } from './components/threed-art/threed-art.component';
+import { AlumniComponent } from './components/alumni/alumni.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+// import { QuestionsComponent } from './components/questions/questions.component';
+import { VideoResourcesComponent } from './components/video-resources/video-resources.component';
+import { SoundResourcesComponent } from './components/sound-resources/sound-resources.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AdminGuard } from './guards/admin.guard';
-import {ArticleComponent} from './Components/article/article.component'
-import {MiniCoursesComponent} from './Components/mini-courses/mini-courses.component'
+import {ArticleComponent} from './components/article/article.component'
+import {MiniCoursesComponent} from './components/mini-courses/mini-courses.component'
 import { AuthGuard } from './guards/auth.guard';
-import { UserProfileComponent } from './Components/user-profile/user-profile.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { ProfileGuard } from './guards/profile.guard';
 
 
 const routes: Routes = [
-  {path:'events', loadChildren: () => import('./Components/events-module/events-module.module').then(m => m.EventsModuleModule)},
+  {path:'events', loadChildren: () => import('./components/events-module/events-module.module').then(m => m.EventsModuleModule)},
   {path: 'register', component: RegisterComponent, canActivate:[AuthGuard]},
   {path: 'privacy', component: PrivacyComponent},
   {path: 'contact', component: ContactUsComponent},
